05263411fd305ae23df58aa7cab8715a451a0841
Happysword <45149700+Happysword@users.noreply.github.com>
2020-07-16 17:51:20 +0200

    Finished excercise 5 and 6

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -20,10 +20,10 @@ grad = zeros(size(theta));
 %
 
 
-
-
-
-
+J = 1/(2*m) * sum( (X*theta - y).^2 ) + lambda/(2*m)*sum(theta(2:end).^2);
+temptheta = theta;
+temptheta(1) = 0;
+grad = (1/m) *  X' * (X*theta - y) + lambda/m *temptheta;
 
 
 
